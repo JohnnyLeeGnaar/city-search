d406ab9474e677fbba236a4c98de50cdbdd575d7
Mario Vuckovic <mario.vuckovic@epti.com>
2020-08-04 10:53:54 +0200

    Another approach

--- index.js
+++ index.js
@@ -1,166 +1,198 @@
-import mockData from './mock-data/cities.js';
+import api from './mock-data/api.js';
 
-const link = "http://api.geonames.org/searchJSON?username=joedoe92&country=hr&maxRows=1000&style=LONG&lang=hr&type=json&cities=cities5000";
-const cities = [...mockData.JSON.geonames];
-let sortedCities = cities;
-let filteredCities = cities;
-let indexedCities;
-let loading = true;
 //const data = fetch(link).then(blob => blob.json()).then(data => cities.push(...data.geonames)).then(loading = true);
-const userList = document.querySelector('.city-list');
-const userInput = document.querySelector('.search');
-const tableHeaders = document.querySelectorAll('[data-val]');
-const btn_next = document.querySelector(".btn_next");
-const btn_prev = document.querySelector(".btn_prev");
-const spanInfo = document.querySelector(".spanInfo");
-const options = document.querySelectorAll('.selectToggle');
-let html;
-let changeDirection = false;
-let optionValue = 0;
-let pages = 0;
-let maxPages = 0;
-let sum = 0;
+const tableBodyEl = document.querySelector('.city-table-body');
+const searchEl = document.querySelector('.search');
+const nextPageEl = document.querySelector(".btn_next");
+const prevPageEl = document.querySelector(".btn_prev");
+const pageSizeEl = document.querySelector('.page-size');
+const tablePaginationEl = document.querySelector('.table-pagination');
+const tableHeaderRowEl = document.querySelector('.city-table-header-row');
 
+let state = {
+	page: 1,
+	pages: null,
+	pageSize: 10,
+	sortBy: 'name',
+	sortOrder: 'asc',
+	query: ''
+};
 
-
-
-
-function findPlaces(cityInput, cities){
-	const regex = new RegExp(cityInput, 'i');
-
-	return cities.filter(city => city.name.match(regex))
-
-}
-function mapCities(cities){
-	let mapArr = cities.map(city => {
-	return `
-	 <tr>
-		 <td>${city.name}</td>
-		 <td>${city.population}</td>
-		 <td>${city.adminName1}</td>
-	</tr>
-	`
-    }).join("");
-
-    return mapArr;
-}
-
-function displayPlaces(e){
-
-	filteredCities = cities;
-    filteredCities = findPlaces(this.value, cities);
-	html = mapCities(filteredCities);
-
- 	userList.innerHTML = html;
-}
-function loadOn(){
-	html = mapCities(cities);
-	userList.innerHTML = html;
-}
-
-function propCompare(value, direction){
-	if(direction === true){
-	return function (a, b){
-		return value === 'population' ? a[value] < b[value] ? 1 : -1 : a[value].localeCompare(b[value]);
-
+let headingItems = [
+	{
+		name: 'City name',
+		value: 'name',
+		active: true,
+		sort: 'asc'
+	},
+	{
+		name: 'Population',
+		value: 'population',
+		active: false,
+		sort: 'none'
+	},
+	{
+		name: 'County',
+		value: 'adminName1',
+		active: false,
+		sort: 'none'
 	}
-}
-	return function (a, b){
-		return value === 'population' ? a[value] > b[value] ? 1 : -1 : b[value].localeCompare(a[value]);
-}}
+]
 
-function decodeHtmlCharCodes(str) { 
-  return str.replace(/(&#(\d+);)/g, function(match, capture, charCode) {
-    return String.fromCharCode(charCode);
-  });
-}
+const getResults = () => {
+	const { query, page, pageSize, sortBy, sortOrder } = state;
 
-function headerSort(e){
-	sortedCities = filteredCities.sort(propCompare(this.dataset.val, changeDirection));
-	let icon;
-	let innerText = this.innerText
+	const { data, metadata: { pages } } = api(query, page, pageSize, sortBy, sortOrder);
 
-	html = mapCities(sortedCities);
-	userList.innerHTML = html;
-	//paginate(sortedCities);
-	icon = !changeDirection ? decodeHtmlCharCodes('&#10506;') : decodeHtmlCharCodes('&#10507;')
-	this.innerText = this.dataset.name + icon;
-	changeDirection = !changeDirection;
-	this.classList.remove('active');
+	state.pages = pages;
+
+	renderTable(data);
 }
 
-
-function dropDownValue(e){
-	optionValue = this.value;
-	btn_next.dataset.option = optionValue;
-	btn_prev.dataset.option = -optionValue;
-	pages = 0;
-	sum = 0;
+const renderTable = (bodyItems) => {
+	const { page, pages } = state;
+	renderTableHeadings(tableHeaderRowEl, headingItems);
+	renderTableBody(bodyItems);
+	renderTablePagination(tablePaginationEl, page, pages);
 }
 
-function previousPage(){
-  if(pages > 0){
-   return pages--;
-  }
-}
-function nextPage(){
-  if(pages<maxPages){
-   return pages++;
-  }
-}
+const renderTableHeadings = (parentEl, items) => {
+	parentEl.innerHTML = '';
+	items.map(item => {
+		const el = document.createElement('th');
+		el.classList.add('city-table-header-column');
+		el.innerText = item.name;
+		el.onclick = () => sortBy(item.value, nextSortOrder(item.sort));
 
+		if (item.active) el.classList.add('active');
+		if (item.sort === 'asc') {
+			el.classList.add('asc');
+		} else if (item.sort == 'desc') {
+			el.classList.add('desc');
+		} else {
+			el.classList.add('asc', 'desc');
+		}
 
-function paginate(){
-	console.log(filteredCities);
-  let indexed;
-  let parseToInt = parseInt(this.dataset.option)
-  maxPages = Math.ceil(filteredCities.length / btn_next.dataset.option);
-  if (pages < 0) pages = 0;
-  if (pages > maxPages) pages = maxPages;
-
-  if(this.name === "prev"){
-	previousPage();
-	indexed = filteredCities.slice(Math.abs(this.dataset.option * pages), sum );
-  }
-  else{
-	nextPage()
-	indexed = filteredCities.slice(sum, this.dataset.option * pages);
-	} 
-   
-  html = mapCities(indexed);
-  userList.innerHTML = html;
-  spanInfo.textContent = `${pages}/${maxPages}`;
-
-console.log(`${this.name} and ${pages} pages / ${maxPages}`)
-
- if (pages == 0) {
-        btn_prev.style.visibility = "hidden";
-    } else {
-        btn_prev.style.visibility = "visible";
-    }
-
-    if (pages == maxPages) {
-        btn_next.style.visibility = "hidden";
-    } else {
-        btn_next.style.visibility = "visible";
-    }
-
-sum += parseToInt;
+		return el;
+	}).forEach(el => parentEl.appendChild(el));
 }
 
-userInput.addEventListener('change', displayPlaces);
-userInput.addEventListener('keyup', displayPlaces);
-tableHeaders.forEach(tableHeader => tableHeader.addEventListener('click', headerSort));
-options.forEach(option => option.addEventListener('change', dropDownValue));
-btn_prev.addEventListener('click', paginate);
-btn_next.addEventListener('click', paginate);
-spanInfo.addEventListener('change',paginate)
+const renderTableBody = (items) => {
+	tableBodyEl.innerHTML = '';
 
+	tableBodyEl.innerHTML = items.map(item => {
+		return `
+			<tr>
+				<td>${item.name}</td>
+				<td>${item.population}</td>
+				<td>${item.adminName1}</td>
+			</tr>
+		`;
+	}).join('');
+}
 
+const renderTablePagination = (parentEl, page, pages) => {
+	const prevButtonEl = document.createElement('button');
+	const nextButtonEl = document.createElement('button');
+	const pageNumbers = [];
 
+	for(let i = 1; i <= pages; i++) {
+		const el = document.createElement('div');
 
-window.addEventListener('load', () =>{
-	if(!loading) return console.error('something went wrong')
-	 setTimeout(loadOn, 100)
+		if(page == i) {
+			el.innerHTML = `<b>${i}</b>`;
+		} else {
+			el.innerHTML = i;
+			el.onclick = () => showPage(i);
+		}
+
+		el.classList.add('pagination-page');
+
+		pageNumbers.push(el);
+	}
+
+	prevButtonEl.classList.add('btn_prev');
+	prevButtonEl.innerText = 'Previous';
+
+	nextButtonEl.classList.add('btn_next');
+	nextButtonEl.innerText = 'Next';
+
+	if (page == 1) {
+		prevButtonEl.setAttribute('disabled', true);
+	} else {
+		prevButtonEl.onclick = () => showPage(page - 1);
+	}
+
+	if (page == pages) {
+		nextButtonEl.setAttribute('disabled', true);
+	} else {
+		nextButtonEl.onclick = () => showPage(page + 1);
+	}
+
+	parentEl.innerHTML = '';
+
+	parentEl.appendChild(prevButtonEl);
+	pageNumbers.forEach(number => parentEl.appendChild(number));
+	parentEl.appendChild(nextButtonEl);
+}
+
+const nextSortOrder = (current) => {
+	let next;
+	console.log(current)
+	if (current == 'none') {
+		next = 'asc';
+	} else if (current == 'asc') {
+		next = 'desc';
+	} else if (current == 'desc') {
+		next = 'asc';
+	}
+
+	return next;
+}
+
+const findBy = event => {
+	const value = event.target.value || '';
+
+	state.page = 1;
+	state.query = value;
+
+	getResults();
+}
+
+const sortBy = (name, order) => {
+	state.page = 1;
+	state.sortOrder = order;
+	state.sortBy = name;
+
+	headingItems.forEach(item => {
+		if (item.value === name) {
+			item.active = true;
+			item.sort = order;
+		} else {
+			item.active = false;
+			item.sort = 'none';
+		}
+	})
+
+	getResults();
+};
+
+const setPageSize = event => {
+	state.pageSize = parseInt(event.target.value);
+	state.page = 1;
+	getResults();
+}
+
+const showPage = page => {
+	console.log(page);
+	state.page = page;
+	getResults();
+}
+
+window.addEventListener('load', () => {
+	pageSizeEl.addEventListener('change', setPageSize);
+	searchEl.addEventListener('change', findBy);
+
+	getResults();
 });
 
